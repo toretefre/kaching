8971b1c60d9701868a30bdebc012da9ae368fa0d
Tore Stensaker Tefre <tore@tefre.it>
2017-12-23 21:38:37 +0100

    modified models

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, unique=True)
     mail = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
@@ -16,21 +16,27 @@ class Customer(models.Model):
 
 
 class Transaction(models.Model):
-    id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True, unique=True)
+    customer = models.ForeignKey(
+        'Customer',
+        on_delete=models.CASCADE,
+    )
+
+    # Connects with multiple items
+    items = models.ManyToManyField(Item)
+
     # For use as shopping cart: False until transaction is completed
     checked_out = models.BooleanField(models.BooleanField(default=False))
-    # Multiple items should be possible
-    # Total amount should be calculated in some way
 
     def __str__(self):
         return self.id
 
 
 class Item(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=200)
     manufacturer = models.CharField(max_length=200)
+    price = models.PositiveIntegerField(max_length=6)
     EAN = models.PositiveIntegerField(max_length=13)
     active_item = models.BooleanField(default=True)
 
