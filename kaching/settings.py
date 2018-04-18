fa3b9508b05617f701bdc14c448e76a85ac80d90
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-04-18 15:11:13 +0200

    disabled validators

--- kaching/settings.py
+++ kaching/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         # 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
         # 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+'''
 
 
 # Internationalization
