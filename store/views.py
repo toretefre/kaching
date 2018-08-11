a14d02e8c5c30be09691f3a35f360872f1eb2132
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-08-11 17:17:27 +0200

    changed weird formatting error

--- store/views.py
+++ store/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
+
 # Create your views here.
 
 
 def itemSelection(request):
-	item = Item.objects.all()
-	return render(
-		request,
-		'itemSelection.html',
-		{'items': item}
-	)
+    item = Item.objects.all()
+    return render(
+        request,
+        'itemSelection.html',
+        {'items': item}
+    )
 
 
 def customerSelection(request):
-	customer = Customer.objects.all()
-	return render(
-		request,
-		'customerSelection.html',
-		{'customers': customer}
-	)
+    customer = Customer.objects.all()
+    return render(
+        request,
+        'customerSelection.html',
+        {'customers': customer}
+    )
